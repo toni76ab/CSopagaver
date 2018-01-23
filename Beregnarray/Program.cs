b4dd1c29b29a401b1dba9f1501779a5f1235afea
toni76ab <admin@TA-8211-06>
2018-01-23 15:41:31 +0100

    after Overload

---
+++ Beregnarray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beregnarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 10, 7, 6, 3, 1, 50, 3 };
+            var res = BeregnOgSorterArray(test);
+
+            Console.WriteLine(res.sum);
+            Console.WriteLine(res.gennemsnit);
+            System.Console.ReadKey();
+
+        }
+
+        public static ArrayResultat BeregnOgSorterArray(int[] a)
+        {
+            ArrayResultat r;
+            r.sum = 0;
+
+            foreach (var item in a)
+            {
+                r.sum += item;
+            }
+            r.gennemsnit = r.sum / a.Length;
+            Array.Sort(a);
+            return r;
+          
+        }
+    }
+
+    struct ArrayResultat
+    {
+        public double sum;
+        public double gennemsnit;
+    }
+}
---
+++ metodeopgave/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodeopgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res);
+
+            double res2 = BeregnAreal(5);
+            Console.WriteLine(res2);
+
+            Udskriv("Dette er en test");
+
+            int[] løn = { 10000, 5000, 30000 };
+            double gns = Gennemsnit(løn);
+            Console.WriteLine(gns);
+
+            System.Console.ReadKey();
+        }
+
+        static int LægSammen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double BeregnAreal(int radius)
+        {
+            return radius * radius * System.Math.PI;
+        }
+
+        static void Udskriv(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+        static double Gennemsnit(int[] månedsløn)
+        {
+            double sum = 0;
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                sum += månedsløn[i];
+            }
+            return sum / månedsløn.Length;
+        }
+
+    }
+}
