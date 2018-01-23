84a5cbc677fc770b58446a9543f6a726c605b3bd
toni76ab <admin@TA-8211-06>
2018-01-23 13:16:00 +0100

    efter løkker

--- løkker/Program.cs
+++ løkker/Program.cs
@@ -10,6 +10,21 @@ namespace løkker
     {
         static void Main(string[] args)
         {
+            for (int xx = 1; xx < 11; xx++)
+            {
+                for (int yy = 1; yy < 11; yy++)
+                {
+                    string tal = (xx * yy).ToString();
+                    if ((xx * yy) > 50)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(tal.PadLeft(4));
+                    //Console.Write(tal);
+                }
+                Console.WriteLine();
+            }
+            System.Console.ReadKey();
         }
     }
 }
