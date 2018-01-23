48b769adde32d170473ca96f3854da07ff6147ec
toni76ab <admin@TA-8211-06>
2018-01-23 12:24:47 +0100

    test

---
+++ Array/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] månedsløn = {13000, 22000, 25000, 26000, 30000, 15000 };
+            double sum = 0;
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                sum += månedsløn[i];
+            }
+            double gns = sum / månedsløn.Length;
+
+            for(int i = 0; i < månedsløn.Length; i++)
+            {
+                Console.WriteLine(månedsløn[i]);
+            }
+            Console.WriteLine("");
+
+            //Array.Sort(månedsløn);
+            System.Array.Sort(månedsløn);
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                Console.WriteLine(månedsløn[i]);
+            }
+            Console.WriteLine("");
+
+            månedsløn.Reverse();
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                Console.WriteLine(månedsløn[i]);
+            }
+            Console.WriteLine("");
+
+            Console.WriteLine("Gennemsnit " + gns.ToString("N2"));
+            //System.Console.Write("Press any key to continue . . . ");
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ Strenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+            string samletNavn = fornavn + " " + efternavn;
+            string navnStort = samletNavn.ToUpper();
+            string navnLille = samletNavn.ToLower();
+            string del = samletNavn.Substring(8,3);
+
+            Console.WriteLine(del);
+            System.Console.ReadKey();
+
+
+        }
+    }
+}
