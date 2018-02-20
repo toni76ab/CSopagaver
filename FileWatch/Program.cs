2258507d9e1eefee71a2eafa217aa65c810a7d1e
admin <toni76ab@gmail.com>
2018-02-20 15:45:00 +0100

    end of day

---
+++ FileWatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.FileSystemWatcher w = new System.IO.FileSystemWatcher(@"c:\temp", "*.txt");
+            w.EnableRaisingEvents = true;
+            w.NotifyFilter = System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.Size;
+            w.Changed += W_Changed;
+            w.Deleted += W_Deleted;
+            do { } while (true);
+
+        }
+
+        private static void W_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.Name + " was deleted");
+        }
+
+        private static void W_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.Name + " was changed");
+        }
+    }
+
+}
